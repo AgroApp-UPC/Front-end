8ca432c9e70501531cc5cd2238cc58c5b282455f
Shukaritas <165834465+Shukaritas@users.noreply.github.com>
2025-10-06 19:30:14 -0500

    featured: db.json and tasks
    Added

--- src/app/shared/presentation/views/my-tasks/my-tasks.component.ts
+++ src/app/shared/presentation/views/my-tasks/my-tasks.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Task } from '../../../../plants/domain/model/task.entity';
+import { TaskService } from '../../../../plants/services/task.services';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-my-tasks',
   standalone: true,
-  template: `
-    <div class="page-container">
-      <h1>My Tasks</h1>
-      <p>Manage your tasks here</p>
-    </div>
-  `,
-  styles: [`
-    .page-container {
-      padding: 2rem;
-    }
-  `]
+  imports: [CommonModule, MatIconModule, MatButtonModule,TranslateModule],
+  templateUrl: './my-tasks.component.html',
+  styleUrls: ['./my-tasks.component.css']
 })
-export class MyTasksComponent {}
+export class MyTasksComponent implements OnInit {
+
+  public tasks$!: Observable<Task[]>;
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasks$ = this.taskService.getTasks();
+  }
+}
